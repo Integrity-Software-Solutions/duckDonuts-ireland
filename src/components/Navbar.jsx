0c2c8f7b3b1d6720a9e9d5c339337328690b1df6
jaredlcox <jlcox2021@gmail.com>
2024-10-24 13:00:19 -0400

    Added the Menudropdown component to the Navbar component.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import OrderOnlineButton from "./NavbarComponents/OrderOnlineButton";
 import DuckDonutsLogo from "../assets/images/NavbarImages/DuckDonutsLogo.png";
+import MenuDropdown from "./NavbarComponents/MenuDropdown";
 
 const Navbar = () => {
   const [hovered, setHovered] = useState("");
@@ -16,11 +17,12 @@ const Navbar = () => {
         <img
           src={DuckDonutsLogo}
           alt="Duck Donuts Logo"
-          className="h-[4.7rem] absolute top-4 left-5"
+          className="h-[4.7rem] absolute top-4 left-5 lg:left-[5.5rem] lg:h-[4.6rem]"
         />
       </Link>
       <div className="mr-8 h-[4.2rem] flex-1 self-end text-white font-bold">
         <div className="flex justify-end h-full pr-3 space-x-3">
+          {/* Home */}
           <div
             className="h-full p-1 hover:cursor-pointer"
             onMouseEnter={() => {
@@ -31,15 +33,16 @@ const Navbar = () => {
             }}
           >
             <p
-              className={`${
-                hovered === "home" ? "border-b-white border-b-[3px]" : ""
-              } py-1`}
+              className={`py-1 border-b-[3.5px] transition-all ease-in-out duration-700 ${
+                hovered === "home" ? "border-b-white" : "border-b-transparent"
+              }`}
             >
               home
             </p>
           </div>
+          {/* Menu */}
           <div
-            className="h-full p-1 hover:cursor-pointer"
+            className="h-full p-1 hover:cursor-pointer relative"
             onMouseEnter={() => {
               handleHover("menu");
             }}
@@ -48,12 +51,13 @@ const Navbar = () => {
             }}
           >
             <p
-              className={`${
-                hovered === "menu" ? "border-b-white border-b-[3px]" : ""
-              } py-1`}
+              className={`py-1 border-b-[3.5px] transition-all ease-in-out duration-700 ${
+                hovered === "menu" ? "border-b-white" : "border-b-transparent"
+              }`}
             >
               menu
             </p>
+            <MenuDropdown hovered={hovered} />
           </div>
         </div>
       </div>
--- src/components/NavbarComponents/MenuDropdown.jsx
+++ src/components/NavbarComponents/MenuDropdown.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
 
-const MenuDropdown = () => {
+const MenuDropdown = (props) => {
+  const menuCategories = [
+    "seasonal",
+    "fan favorites",
+    "everyday assortments",
+    "coffee, espresso, & more",
+    "ice cream",
+    "lemonade & refreshers",
+    "sandwiches",
+    "made-to-order donuts",
+  ];
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div
+        className={`${
+          props.hovered === "menu"
+            ? "transition-all ease-in-out duration-700 absolute bottom-0 left-0 w-auto h-1 bg-black opacity-5 rounded-sm min-w-[16em]"
+            : "opacity-0"
+        }`}
+      ></div>
 
-export default MenuDropdown
+      {/* Main dropdown content */}
+      <div
+        className={`transition-all ease-in-out duration-500 absolute top-[4.2rem] left-0 w-auto bg-sky-500 shadow-2xl px-6 py-4 min-w-[16em] space-y-2 ${
+          props.hovered === "menu"
+            ? "opacity-100 translate-y-0"
+            : "opacity-0 translate-y-0"
+        }`}
+      >
+        {menuCategories.map((category, index) => (
+          <p
+            key={index}
+            className="text-white py-1 hover:cursor-pointer hover:border-b-[3.5px] hover:border-b-white"
+          >
+            {category}
+          </p>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default MenuDropdown;
