4ee57ed4c200a7c016ed42a702ceb07420253653
jaredlcox <jlcox2021@gmail.com>
2024-11-15 12:28:40 -0500

    Added a new DuckDonutsMobileLogo for mobile devices. Added a variant to the original Navbar for smaller screen sizes. Created a MenuToggle.jsx component that transitions from a hamburger icon to an x.

---
+++ src/components/NavbarComponents/MenuToggle.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+const MenuToggle = () => {
+  const [isChanged, setIsChanged] = useState(false);
+
+  const handleClick = () => {
+    setIsChanged((prev) => !prev);
+  };
+  return (
+    <div
+      className={`container ${
+        isChanged ? "change" : ""
+      } inline-block cursor-pointer w-auto`}
+      onClick={handleClick}
+    >
+      <div
+        className="bar1"
+        style={{
+          width: "35px",
+          height: "5px",
+          backgroundColor: "white",
+          margin: "6px 0",
+          transition: "0.4s",
+          transform: isChanged ? "translateY(11px) rotate(-135deg)" : "none",
+        }}
+      ></div>
+      <div
+        className="bar2"
+        style={{
+          width: "35px",
+          height: "5px",
+          backgroundColor: "white",
+          margin: "6px 0",
+          transition: "0.4s",
+          opacity: isChanged ? 0 : 1,
+        }}
+      ></div>
+      <div
+        className="bar3"
+        style={{
+          width: "35px",
+          height: "5px",
+          backgroundColor: "white",
+          margin: "6px 0",
+          transition: "0.4s",
+          transform: isChanged ? "translateY(-11px) rotate(135deg)" : "none",
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default MenuToggle;
