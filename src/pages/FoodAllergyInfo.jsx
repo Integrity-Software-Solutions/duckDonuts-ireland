25202a9af5e29e9c4a00f34ed6ddd7c7f2a36c0a
jaredcox22 <jcox@ircuwd.com>
2024-11-20 14:07:59 -0500

    Updated the FoodAllergyInfo component to be more responsive to various screen sizes.

--- src/pages/FoodAllergyInfo.jsx
+++ src/pages/FoodAllergyInfo.jsx
@@ -26,37 +26,39 @@ const FoodAllergyInfo = () => {
           }}
         />
         <div
-          className="uppercase absolute bottom-[7rem] left-[22%] text-6xl font-bold text-white font-boogaloo w-1/2 leading-snug"
+          className="uppercase absolute bottom-[2.5rem] left-[10%] lg:bottom-[7rem] lg:left-[22%] text-2xl sm:text-4xl md:text-5xl text-nowrap lg:text-6xl font-bold text-white font-boogaloo w-1/2 leading-snug"
           style={{ textShadow: "6px 6px 12px rgba(0, 0, 0, 0.7)" }}
         >
           {title}
         </div>
       </div>
-      <div className="flex flex-col justify-self-center h-full w-full max-w-[72rem] my-24 mx-20">
-        <div className="text-7xl uppercase font-boogaloo text-sky-500">
+      <div className="flex flex-col justify-self-center h-full w-full max-w-[72rem] py-10 sm:my-16 xl:my-24 mx-20 px-6 xl:px-0">
+        <div className="text-2xl sm:text-4xl md:text-5xl lg:text-7xl text-nowrap uppercase font-boogaloo text-sky-500">
           {title}
         </div>
-        <div className="mt-6 text-lg">
+        <div className="mt-6 text-lg font-semibold md:font-normal">
           Major food allergens are or may be present in our products, including
           wheat, egg, dairy, soy, peanuts and tree nuts.
         </div>
-        <div className="mt-6 text-lg">
+        <div className="mt-6 text-lg font-semibold md:font-normal">
           If you have a food allergy or ingredient concern, please alert a team
           member. We are not able to 100% guarantee cross-contact has not
           occurred in-store or in manufacturing facilities.
         </div>
         <div className="mt-10 text-xl font-bold">Nutrition Data</div>
-        <div className="mt-6 text-lg flex">
-          <a
-            href="/documents/Nutrition-Allergy-Info.pdf"
-            target="_blank"
-            className="text-sky-500 underline hover:no-underline hover:text-[#E6427A] font-bold cursor-pointer mr-1"
-          >
-            Click this link
-          </a>
+        <p className="mt-6 text-lg font-semibold md:font-normal">
+          <strong>
+            <a
+              target="_blank"
+              href="/documents/Nutrition-Allergy-Info.pdf"
+              className="text-sky-500 underline hover:no-underline hover:text-[#E6427A] font-bold cursor-pointer mr-1"
+            >
+              Click this link
+            </a>
+          </strong>
           to download a pdf of our nutrition and allergy guide.
-        </div>
-        <div className="mt-6 text-lg">
+        </p>
+        <div className="mt-6 text-lg font-semibold md:font-normal">
           For more information, contact your local Duck Donuts!
         </div>
       </div>
