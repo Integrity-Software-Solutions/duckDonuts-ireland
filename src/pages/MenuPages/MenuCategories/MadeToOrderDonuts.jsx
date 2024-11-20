33ed404cafe672b02dd8ba42c4c274ea5f450a4f
jaredcox22 <jcox@ircuwd.com>
2024-11-20 16:17:29 -0500

    Updated the MadeToOrderDonuts page to be more responsive to various screen sizes.

--- src/pages/MenuPages/MenuCategories/MadeToOrderDonuts.jsx
+++ src/pages/MenuPages/MenuCategories/MadeToOrderDonuts.jsx
@@ -28,38 +28,42 @@ const MadeToOrderDonuts = () => {
           }}
         />
         <div
-          className="uppercase absolute bottom-[7rem] left-[22%] text-6xl font-bold text-white font-boogaloo"
+          className="uppercase absolute bottom-[2.5rem] left-[10%] lg:bottom-[7rem] lg:left-[22%] text-2xl sm:text-4xl md:text-5xl text-nowrap lg:text-6xl font-bold text-white font-boogaloo"
           style={{ textShadow: "6px 6px 12px rgba(0, 0, 0, 0.7)" }}
         >
           {title1}
         </div>
       </div>
-      <div className="flex flex-col justify-self-center h-full w-full max-w-[75rem] my-24 mx-20">
+      <div className="flex flex-col justify-self-center h-full w-full max-w-[75rem] py-10 sm:my-16 xl:my-24 mx-20 px-6 xl:px-0">
         <div
-          className={` text-sky-500 text-8xl font-bold font-boogaloo`}
+          className={` text-sky-500 text-5xl lg:text-7xl font-bold font-boogaloo`}
           style={{ textShadow: "2px 2px 4px rgb(0, 0, 0, 0.5)" }}
         >
           {title2}
         </div>
-        <div className="mt-8 mb-7 uppercase text-3xl font-bold">Our secret to delicious donuts</div>
-        <div className="text-xl font-light">
+        <div className="mt-8 mb-7 uppercase text-2xl md:text-3xl font-bold">
+          Our secret to delicious donuts
+        </div>
+        <div className="font-semibold md:text-xl md:font-light">
           It all begins wiht our signatrue vanilla cake donut, freshly made the
           moment you order. The rest is up to you!
         </div>
-        <div className="font-bold text-lg py-6">1. Start with a Coating.</div>
-        <div className="text-xl font-light">
+        <div className="font-bold md:text-lg py-6">
+          1. Start with a Coating.
+        </div>
+        <div className="font-semibold md:text-xl md:font-light">
           Bare, Cinnamon Sugar, Powdered Sugar, Glazed, Chocolate Icing, Vanilla
           Icing, Strawberry Icing, Peanut Butter Icing, Maple Icing, Lemon
           Icing, Blueberry Icing
         </div>
-        <div className="font-bold text-lg py-6">2. Pick a Topping.</div>
-        <div className="text-xl font-light">
+        <div className="font-bold md:text-lg py-6">2. Pick a Topping.</div>
+        <div className="font-semibold md:text-xl md:font-light">
           Rainbow Sprinkles, Chocolate Sprinkles, Graham Cracker Crumbs, chopped
           Peanuts, Chopped Bacon, Shredded Coconut, Oreo® Cookie Pieces,
           Streusel
         </div>
-        <div className="font-bold text-lg py-6">3. Choose a Drizzle.</div>
-        <div className="text-xl font-light">
+        <div className="font-bold md:text-lg py-6">3. Choose a Drizzle.</div>
+        <div className="font-semibold md:text-xl md:font-light">
           Hot Fudge, Marshmallow, Salted Caramel, Raspberry, or pick from any
           icing above!
         </div>
